fe26951867833eabd0f20d20033d6652d7357a17
RaphMar2022 <raphmar2022@hotmail.com>
2023-04-19 23:40:57 +0200

    bmps

--- kernel/src/libasg/libasg.h
+++ kernel/src/libasg/libasg.h
@@ -17,3 +17,10 @@ typedef struct
 } Bitmap;
 
 void init_libasg(multiboot_info_t *info);
+void set_pixel(int x, int y, Color color);
+Color get_pixel(int x, int y);
+void set_rect(int x, int y, int width, int height, Color color);
+void set_round_rect(int x, int y, int width, int height, int radius, Color color);
+void set_bitmap(int x, int y, Bitmap b);
+void set_circle(int x0, int y0, int radius, Color color);
+void flush();
