3858a61d2e56d07e8118935b3e8ef2732cc8f355
RaphMar2022 <raphmar2022@hotmail.com>
2023-04-19 17:28:50 +0200

    test os stability + new resource file

---
+++ kernel/src/res/res.h
@@ -0,0 +1,12 @@
+#include <stdint.h>
+
+#define ASTRALOGO_WIDTH 64
+#define ASTRALOGO_HEIGHT 64
+#define SUCC_WIDTH 121
+#define SUCC_HEIGHT 70
+#define DVDVIDEOLOLOL_HEIGHT 151
+#define DVDVIDEOLOLOL_WIDTH 213
+
+extern uint32_t dvdvideololol[];
+extern uint32_t succ[];
+extern uint32_t astralogo[];
